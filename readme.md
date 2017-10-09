05877c3dd413e1d5d365893ab1b9cf2c244684f7
Yan Terentiev <y.terentiev@papakaya.com>
2017-10-09 13:36:50 +0300

    chamge readme

---
+++ readme.md
@@ -0,0 +1,1 @@
+Well, something like this, if without transactions )
--- routes/api.php
+++ routes/api.php
@@ -11,10 +11,6 @@
 |
 */
 
-//Route::middleware('auth:api')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-
 Route::get('/balance/', 'UserController@showBalance');
 
 Route::post('/deposit', 'UserController@deposit');
